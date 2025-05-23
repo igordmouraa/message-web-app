7c5d9d0a51a74a6edfcc46254af7ead8c3b114f7
igordmouraa <igordmoura_@hotmail.com>
2025-05-23 11:42:09 -0300

    feat: add Open Graph and Twitter card metadata for improved social sharing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,35 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import type {Metadata} from 'next'
+import {Inter} from 'next/font/google'
 import './globals.css'
+import React from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export const metadata: Metadata = {
     title: 'Message Web App',
     description: 'Aplicativo de mensagens em tempo real',
+    openGraph: {
+        url: 'https://message-web-app-ten.vercel.app/',
+        siteName: 'Message Web App',
+        images: [
+            {
+                url: '/website.png',
+                width: 1200,
+                height: 630,
+                alt: 'Imagem de preview do app',
+            },
+        ],
+        type: 'website',
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title: 'Message Web App',
+        description: 'Converse em tempo real com seus amigos!',
+        images: ['/website.png'],
+    },
+    icons: {
+        icon: '/message-square.svg',
+    },
 }
 
 export default function RootLayout({
